0cd4f03464576476b0e6e3bf462e9214be8ff531
Nick <nick@antikode.com>
2014-01-28 01:42:06 +0700

    TEst

--- pages_/contact/index.php
+++ pages_/contact/index.php
@@ -39,48 +39,61 @@ if(isset($_POST['btn_contact'])){
 
           
             <h2 class="m_b_20 text-right">Contact</h2>
+
+            <div class="row">
             
-			<?php 
-			// ALERT NOTIFICATION
-			
-			if(!empty($_SESSION['alert'])){
-			   echo '<div class="alert-'.$_SESSION['alert'].'">'.$_SESSION['msg'].'</div>';
-			}
-			
-			if($_POST['btn_contact'] == ""){
-			   unset($_SESSION['alert']);
-			   unset($_SESSION['msg']);
-			}
-			?>
-            
-            <form role="form" method="post">
-              <div class="form-group row">
-                <label class="col-xs-3">Name</label>
-                <div class="col-xs-9">
-                  <input type="text" class="form-control" name="contact_name" id="id_contact_name">
-                </div>
+        			<?php 
+        			// ALERT NOTIFICATION
+        			
+        			if(!empty($_SESSION['alert'])){
+        			   echo '<div class="alert-'.$_SESSION['alert'].'">'.$_SESSION['msg'].'</div>';
+        			}
+        			
+        			if($_POST['btn_contact'] == ""){
+        			   unset($_SESSION['alert']);
+        			   unset($_SESSION['msg']);
+        			}
+        			?>
+
+              <div class="col-xs-4">
+                asdasd
               </div>
-              <div class="form-group row">
-                <label class="col-xs-3">Email</label>
-                <div class="col-xs-9">
-                  <input type="email" class="form-control" name="contact_email" id="id_contact_email">
-                </div>
-              </div>
-              <div class="form-group row">
-                <label class="col-xs-3">Subject</label>
-                <div class="col-xs-9">
-                  <input type="text" class="form-control" name="contact_subject" id="id_contact_subject">
-                </div>
-              </div>
-              <div class="form-group row">
-                <label class="col-xs-3">Message</label>
-                <div class="col-xs-9">
-                  <textarea class="form-control" rows="5" name="contact_msg" id="id_contact_msg"></textarea>
-                </div>
-              </div>
-              <input type="button" class="btn btn-default pull-right " value="Submit" onclick="validation()" id="btn_alias">
-              <input type="submit" class="hidden" value="Submit" name="btn_contact" id="id_btn_contact">
-            </form>
+              
+              <div class="col-xs-8">
+
+                <form role="form" method="post">
+                  <div class="form-group row">
+                    <label class="col-xs-3 hidden">Name</label>
+                    <div class="col-xs-12">
+                      <input type="text" class="form-control" name="contact_name" id="id_contact_name" placeholder="Name">
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label class="col-xs-3 hidden">Email</label>
+                    <div class="col-xs-12">
+                      <input type="email" class="form-control" name="contact_email" id="id_contact_email" placeholder="Email">
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label class="col-xs-3 hidden">Subject</label>
+                    <div class="col-xs-12">
+                      <input type="text" class="form-control" name="contact_subject" id="id_contact_subject" placeholder="Subject">
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label class="col-xs-3 hidden">Message</label>
+                    <div class="col-xs-12">
+                      <textarea class="form-control" rows="5" name="contact_msg" id="id_contact_msg" placeholder="Message"></textarea>
+                    </div>
+                  </div>
+                  <input type="button" class="btn btn-default pull-right " value="Submit" onclick="validation()" id="btn_alias">
+                  <input type="submit" class="hidden" value="Submit" name="btn_contact" id="id_btn_contact">
+                </form>
+
+              </div><!--.col-xs-6-->
+
+            </div><!--.row-->
+
           </div>
         </div><!--.row-->
 
